069d2f9071e606b17e1edca8ed03911db7666a61
GSingh1994 <singh.gagandeep0034@gmail.com>
2023-02-26 13:53:13 -0600

    Initial Commit

---
+++ MortyDex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MortyDex
+//
+//  Created by Gagan on 2023-02-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
