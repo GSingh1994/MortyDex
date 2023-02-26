2d71e1a3f035074457efa6981d8aa1e3b20a01f7
GSingh1994 <singh.gagandeep0034@gmail.com>
2023-02-26 14:42:23 -0600

    add Schema folder generated by Apollo-cli

---
+++ MortySchema/Operations/Queries/AllCharactersQuery.graphql.swift
@@ -0,0 +1,86 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+@_exported import ApolloAPI
+
+public extension MortySchema {
+  class AllCharactersQuery: GraphQLQuery {
+    public static let operationName: String = "AllCharacters"
+    public static let document: ApolloAPI.DocumentType = .notPersisted(
+      definition: .init(
+        #"""
+        query AllCharacters($page: Int) {
+          characters(page: $page) {
+            __typename
+            results {
+              __typename
+              name
+              image
+              gender
+            }
+          }
+        }
+        """#
+      ))
+
+    public var page: GraphQLNullable<Int>
+
+    public init(page: GraphQLNullable<Int>) {
+      self.page = page
+    }
+
+    public var __variables: Variables? { ["page": page] }
+
+    public struct Data: MortySchema.SelectionSet {
+      public let __data: DataDict
+      public init(data: DataDict) { __data = data }
+
+      public static var __parentType: ApolloAPI.ParentType { MortySchema.Objects.Query }
+      public static var __selections: [ApolloAPI.Selection] { [
+        .field("characters", Characters?.self, arguments: ["page": .variable("page")]),
+      ] }
+
+      /// Get the list of all characters
+      public var characters: Characters? { __data["characters"] }
+
+      /// Characters
+      ///
+      /// Parent Type: `Characters`
+      public struct Characters: MortySchema.SelectionSet {
+        public let __data: DataDict
+        public init(data: DataDict) { __data = data }
+
+        public static var __parentType: ApolloAPI.ParentType { MortySchema.Objects.Characters }
+        public static var __selections: [ApolloAPI.Selection] { [
+          .field("results", [Result?]?.self),
+        ] }
+
+        public var results: [Result?]? { __data["results"] }
+
+        /// Characters.Result
+        ///
+        /// Parent Type: `Character`
+        public struct Result: MortySchema.SelectionSet {
+          public let __data: DataDict
+          public init(data: DataDict) { __data = data }
+
+          public static var __parentType: ApolloAPI.ParentType { MortySchema.Objects.Character }
+          public static var __selections: [ApolloAPI.Selection] { [
+            .field("name", String?.self),
+            .field("image", String?.self),
+            .field("gender", String?.self),
+          ] }
+
+          /// The name of the character.
+          public var name: String? { __data["name"] }
+          /// Link to the character's image.
+          /// All images are 300x300px and most are medium shots or portraits since they are intended to be used as avatars.
+          public var image: String? { __data["image"] }
+          /// The gender of the character ('Female', 'Male', 'Genderless' or 'unknown').
+          public var gender: String? { __data["gender"] }
+        }
+      }
+    }
+  }
+
+}
---
+++ MortySchema/Schema/SchemaMetadata.graphql.swift
@@ -0,0 +1,46 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+import ApolloAPI
+
+public protocol MortySchema_SelectionSet: ApolloAPI.SelectionSet & ApolloAPI.RootSelectionSet
+where Schema == MortySchema.SchemaMetadata {}
+
+public protocol MortySchema_InlineFragment: ApolloAPI.SelectionSet & ApolloAPI.InlineFragment
+where Schema == MortySchema.SchemaMetadata {}
+
+public protocol MortySchema_MutableSelectionSet: ApolloAPI.MutableRootSelectionSet
+where Schema == MortySchema.SchemaMetadata {}
+
+public protocol MortySchema_MutableInlineFragment: ApolloAPI.MutableSelectionSet & ApolloAPI.InlineFragment
+where Schema == MortySchema.SchemaMetadata {}
+
+public extension MortySchema {
+  typealias ID = String
+
+  typealias SelectionSet = MortySchema_SelectionSet
+
+  typealias InlineFragment = MortySchema_InlineFragment
+
+  typealias MutableSelectionSet = MortySchema_MutableSelectionSet
+
+  typealias MutableInlineFragment = MortySchema_MutableInlineFragment
+
+  enum SchemaMetadata: ApolloAPI.SchemaMetadata {
+    public static let configuration: ApolloAPI.SchemaConfiguration.Type = SchemaConfiguration.self
+
+    public static func objectType(forTypename typename: String) -> Object? {
+      switch typename {
+      case "Query": return MortySchema.Objects.Query
+      case "Characters": return MortySchema.Objects.Characters
+      case "Character": return MortySchema.Objects.Character
+      default: return nil
+      }
+    }
+  }
+
+  enum Objects {}
+  enum Interfaces {}
+  enum Unions {}
+
+}
