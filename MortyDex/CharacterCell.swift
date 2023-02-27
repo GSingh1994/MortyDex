bb5f89dc7f3438e18ca53c809c7b1c44cda3fec3
GSingh1994 <singh.gagandeep0034@gmail.com>
2023-02-27 09:58:52 -0600

    add UICollectionViewController

---
+++ MortyDex/CharacterCell.swift
@@ -0,0 +1,14 @@
+//
+//  CharacterCell.swift
+//  MortyDex
+//
+//  Created by Gagan on 2023-02-27.
+//
+
+import UIKit
+
+class CharacterCell: UICollectionViewCell {
+    @IBOutlet weak var characterImage: UIImageView!
+    @IBOutlet weak var characterName: UILabel!
+    
+}
--- MortyDex/ViewController.swift
+++ MortyDex/ViewController.swift
@@ -11,17 +11,29 @@ import Apollo
 
 
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
     let apolloClient = ApolloClient(url: URL(string: "https://rickandmortyapi.com/graphql")!)
     override func viewDidLoad() {
         super.viewDidLoad()
-        apolloClient.fetch(query: MortySchema.AllCharactersQuery(page: 1)) { result in
-          guard let data = try? result.get().data else { return }
-            print(data.characters?.results![1]?.name) // Luke Skywalker
-        }
+        //        apolloClient.fetch(query: MortySchema.AllCharactersQuery(page: 1)) { result in
+        //          guard let data = try? result.get().data else { return }
+        //            print(data.characters?.results![1]?.name) // Luke Skywalker
+        //        }
         
     }
-
-
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Character", for: indexPath) as! CharacterCell
+        cell.layer.borderColor = UIColor.black.cgColor
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 10
+        return cell
+    }
+    
 }
 
